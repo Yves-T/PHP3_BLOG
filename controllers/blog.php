86132fd391e60fdc98b8ec563cae489a6133784a
Yves Talboom <yves.talboom@gmail.com>
2016-02-12 09:23:51 +0100

    Add comment form

--- controllers/blog.php
+++ controllers/blog.php
@@ -9,6 +9,7 @@ if ($isEntryClicked) {
     $entryId = $_GET ['id'];
     $entryData = $entryTable->getEntry($entryId);
     $blogOutput = include_once "views/entry_html.php";
+    $blogOutput .= include_once "controllers/comments.php";
 } else {
     $entries = $entryTable->getAllEntries();
     $blogOutput = include_once "views/list_entries_html.php";
---
+++ controllers/comments.php
@@ -0,0 +1,10 @@
+<?php
+
+// model
+
+
+// view
+$comments = include_once "views/comment_form_html.php";
+
+// hooking up view and model
+return $comments;
