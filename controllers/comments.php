000e93b13fddc247000ac7954ca0df98704df2fb
Yves Talboom <yves.talboom@gmail.com>
2016-02-12 10:32:54 +0100

    Save comment with comment form

--- controllers/comments.php
+++ controllers/comments.php
@@ -3,6 +3,15 @@
 include_once "models/Comment_Table.class.php";
 //create a new object, pass it a PDO database connection object
 $commentTable = new Comment_Table($db);
+
+$newCommentSubmitted = isset($_POST['new-comment']);
+if ($newCommentSubmitted) {
+    $whichEntry = $_POST['entry-id'];
+    $user = $_POST['user-name'];
+    $comment = $_POST['new-comment'];
+    $commentTable->saveComment($whichEntry, $user, $comment);
+}
+
 $comments = include_once "views/comment_form_html.php";
 
 $allComments = $commentTable->getAllById($entryId);
