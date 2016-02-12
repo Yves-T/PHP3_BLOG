d74fef8a21d48cf665a71d1f06c4ddfb01e6802f
Yves Talboom <yves.talboom@gmail.com>
2016-02-12 10:18:02 +0100

    Add view for listing comments

--- controllers/comments.php
+++ controllers/comments.php
@@ -1,10 +1,12 @@
 <?php
-
-// model
-
-
-// view
+//include class definition
+include_once "models/Comment_Table.class.php";
+//create a new object, pass it a PDO database connection object
+$commentTable = new Comment_Table($db);
 $comments = include_once "views/comment_form_html.php";
+
+$allComments = $commentTable->getAllById($entryId);
+$comments .= include_once "views/comments_html.php";
 
 // hooking up view and model
 return $comments;
